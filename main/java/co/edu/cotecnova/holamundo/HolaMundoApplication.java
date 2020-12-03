049bb8498741f7efadfa25f984abba35fb270dba
viviana <danivivi1205@gmail.com>
2020-12-02 20:44:42 -0500

    creacion hola mundo

---
+++ main/java/co/edu/cotecnova/holamundo/HolaMundoApplication.java
@@ -0,0 +1,7 @@
+package main.java.co.edu.cotecnova.holamundo;
+
+public class HolaMundoApplication {
+    public static void main(String[] args){
+        System.out.println("Hola Mundo");
+    }
+}
---
+++ test/java/co/edu/cotecnova/holamundo/HolaMundoApplicationTest.java
@@ -0,0 +1,4 @@
+package test.java.co.edu.cotecnova.holamundo;
+
+public class HolaMundoApplicationTest {
+}
